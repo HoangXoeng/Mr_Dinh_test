582df6037183923c7f17d19ecfb7f7919c976ffc
Xuan Nguyen <hoangnguyendepgiai@gmail.com>
2024-09-17 17:17:25 +0700

    done

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
+
+const fileInput = document.querySelector("#img_link");
+var dataUrl; 
+fileInput.addEventListener('change', function(event) {
+  const file = event.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      dataUrl = e.target.result; // Đây là DataURL của ảnh
+      // Bây giờ bạn đã có DataURL, bạn có thể gọi hàm để hiển thị ảnh trong JS ở nơi khác
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+
+
 const add = () => {
-  if (!img_link || !product_name || !new_price || !old_price || !product_code) {
+
+  // var img_link = document.querySelector("#");
+  
+  var product_name = document.querySelector("#name").value;
+  var new_price = parseInt(document.querySelector("#newPrice").value);
+  var old_price = parseInt(document.querySelector("#oldPrice").value);
+  var product_code = document.querySelector("#product_code").value;
+  var type = document.querySelector("#type").value;
+  if ( !product_name || !new_price || !old_price || !product_code) {
     alert("Vui lòng nhập đầy đủ thông tin sản phẩm!");
     return;
   }
-  console.log(img_link + product_name + product_code + old_price + new_price);
+ 
 
   var productInfor = " <div class=" + "card" + " style=" + "width:400px" + ">";
-  productInfor +=
-    " <img class=" +
-    "card-img-top" +
-    " src=" +
-    img_link +
-    " alt=" +
-    "Card image cap" +
-    ">";
-  productInfor += " <div class=" + "card-body" + ">";
-  productInfor +=
-    " <h4 class=" +
-    "card-title" +
-    ">" +
-    product_name +
-    " - " +
-    product_code +
-    "</h4>";
-  productInfor += "<div class=" + "price" + ">";
-  productInfor += " <p class=" + "newPrice" + ">" + new_price + "vnđ" + "</p>";
-  productInfor += " <p class=" + "oldPrice" + ">" + old_price + "vnđ" + "</p>";
-  productInfor += "</div>";
-  productInfor +=
-    "<a href=" + "#" + " class=" + "btn btn-primary" + ">Đặt Mua</a>";
-  productInfor += " </div> " + "</div>";
+ productInfor += `
+    <img class="card-img-top" src = "${dataUrl}"id="card-img-top" alt="Card image cap">
+    <div class="card-body">
+        <h4 class="card-title">${product_name} - ${product_code}</h4>
+        <div class="price">
+            <p class="newPrice">${new_price}vnđ</p>
+            <p class="oldPrice">${old_price}vnđ</p>
+        </div>
+        <a href="#" class="btn btn-primary">Đặt Mua</a>
+    </div>
+</div>`;
+
+
+
 
   if (type == "nu") {
     document.querySelector(".body_content_session_girl").innerHTML +=
       productInfor;
+      
+      
   } else {
     document.querySelector(".body_content_session_boy").innerHTML +=
       productInfor;
+    
+      
   }
+    
+  
 };
 
-var img_link = document.querySelector("#img_link").value;
-var product_name = document.querySelector("#name").value;
-var new_price = parseInt(document.querySelector("#newPrice").value);
-var old_price = parseInt(document.querySelector("#oldPrice").value);
-var product_code = document.querySelector("#product_code").value;
-var type = document.querySelector("#type").value;
