b53aef48d3ab2a157805111e7493c011cbc407a8
Xuan Nguyen <hoangnguyendepgiai@gmail.com>
2024-09-17 10:10:31 +0700

    done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const add = () => {
   var img_link = document.querySelector("#img_link").value;
   var product_name = document.querySelector("#name").value;
-  var new_price = document.querySelector("#newPrice").value;
-  var old_price = document.querySelector("#oldPrice").value;
+  var new_price = parseInt(document.querySelector("#newPrice").value);
+  var old_price = parseInt(document.querySelector("#oldPrice").value);
   var product_code = document.querySelector("#product_code").value;
 
   if (!img_link || !product_name || !new_price || !old_price || !product_code) {
@@ -32,7 +32,7 @@ const add = () => {
     "</h4>";
   productInfor += "<div class=" + "price" + ">";
   productInfor += " <p class=" + "newPrice" + ">" + new_price + "vnđ" + "</p>";
-  productInfor += " <p class=" + "oldPrice" + ">" + old_price + "vnđ" + +"</p>";
+  productInfor += " <p class=" + "oldPrice" + ">" + old_price + "vnđ"  +"</p>";
   productInfor += "</div>";
   productInfor +=
     "<a href=" + "#" + " class=" + "btn btn-primary" + ">Đặt Mua</a>";
